ceed24f22ef635461ca43d9de42fad22294bba4a
01jaejae-del <01jaejae@gmail.com>
2025-11-21 02:04:14 +0900

    Add files via upload

---
+++ sample1/sketch.js
@@ -0,0 +1,922 @@
+function setup() {
+  createCanvas(600, 400);
+  background('#000069');
+  
+  stroke('#fafabe');
+  fill('#fafabe');
+  circle(150, 100, 30);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(150, 100, 33);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(150, 100, 37);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(150, 100, 42);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(150, 100, 48);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(150, 100, 55);
+  
+  stroke('#fafabe');
+  noFill();
+  arc(150, 100, 63, 63, radians(-120), radians(180));
+  stroke('#ffffff');
+  noFill();
+  arc(150, 100, 66, 66, radians(-170), radians(-130));
+  
+  stroke('#fafabe');
+  noFill()
+  arc(150, 100, 72, 72, radians(-90), radians(210));
+  stroke('#ffffff');
+  noFill();
+  arc(150, 100, 75, 75, radians(-140), radians(-100));
+  
+  stroke('#fafabe');
+  noFill();
+  arc(150, 100, 82, 82, radians(-60), radians(240));
+  stroke('#ffffff');
+  noFill();
+  arc(150, 100, 85, 85, radians(-110), radians(-70));
+
+  stroke('#fafabe');
+  noFill()
+  arc(150, 100, 93, 93, radians(-30), radians(270));
+  stroke('#ffffff');
+  noFill();
+  arc(150, 100, 96, 96, radians(-80), radians(-40));
+
+  stroke('#fafabe');
+  noFill()
+  arc(150, 100, 105, 105, radians(0), radians(300));
+  stroke('#ffffff');
+  noFill();
+  arc(150, 100, 108, 108, radians(-50), radians(-10));
+  
+  stroke('#fafabe');
+  noFill()
+  arc(150, 100, 118, 118, radians(30), radians(330));
+  stroke('#ffffff');
+  noFill();
+  arc(150, 100, 121, 121, radians(-20), radians(20));
+  
+  stroke('#fafabe');
+  noFill()
+  arc(150, 100, 132, 132, radians(60), radians(360));
+  stroke('#ffffff');
+  noFill();
+  arc(150, 100, 135, 135, radians(10), radians(50));
+  
+  stroke('#fafabe');
+  noFill()
+  arc(150, 100, 147, 147, radians(90), radians(30));
+  stroke('#ffffff');
+  noFill();
+  arc(150, 100, 144, 144, radians(40), radians(80));
+  
+  stroke('#fafabe');
+  noFill()
+  arc(150, 100, 163, 163, radians(120), radians(60));
+  stroke('#ffffff');
+  noFill();
+  arc(150, 100, 160, 160, radians(70), radians(110));
+  
+  stroke('#fafabe');
+  noFill()
+  arc(150, 100, 180, 180, radians(150), radians(90));
+  stroke('#ffffff');
+  noFill();
+  arc(150, 100, 177, 177, radians(100), radians(140));
+  
+  //달에 관한 코트
+  
+  stroke('#fafabe');
+  fill('#fafabe');
+  circle(55, 30, 5);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(55, 30, 8);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(55, 30, 11);
+  
+  //왼쪽상단별
+  
+  stroke('#fafabe');
+  fill('#fafabe');
+  circle(50, 140, 5);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(50, 140, 8);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(50, 140, 11);
+  
+  //왼쪽중단별
+  
+  stroke('#fafabe');
+  fill('#fafabe');
+  circle(70, 220, 5);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(70, 220, 8);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(70, 220, 11);
+  
+  //왼쪽하단별
+  
+  stroke('#fafabe');
+  fill('#fafabe');
+  circle(300, 80, 5);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(300, 80, 8);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(300, 80, 11);
+  
+  //가운데상단별
+  
+  stroke('#fafabe');
+  fill('#fafabe');
+  circle(270, 180, 5);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(270, 180, 8);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(270, 180, 11);
+  
+  //가운데하단별
+  
+  stroke('#fafabe');
+  fill('#fafabe');
+  circle(400, 50, 5);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(400, 50, 8);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(400, 50, 11);
+  
+  //오른쪽상단별
+  
+  stroke('#fafabe');
+  fill('#fafabe');
+  circle(480, 90, 5);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(480, 90, 8);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(480, 90, 11);
+  
+  //오른쪽중단별
+  
+  stroke('#fafabe');
+  fill('#fafabe');
+  circle(550, 20, 5);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(550, 20, 8);
+  
+  stroke('#fafabe');
+  noFill();
+  circle(550, 20, 11);
+  
+  //오른쪽하단별
+  //큰별관련코드
+  
+  stroke('#ffffff');
+  fill('#ffffff');
+  circle(30, 80, 3);
+  
+  stroke('#ffffff');
+  noFill();
+  circle(30, 80, 6);
+  
+  //왼쪽작은별
+  
+  stroke('#ffffff');
+  fill('#ffffff');
+  circle(270, 30, 3);
+  
+  stroke('#ffffff');
+  noFill();
+  circle(270, 30, 6);
+  
+  //중간상단작은별
+  
+  stroke('#ffffff');
+  fill('#ffffff');
+  circle(300, 150, 3);
+  
+  stroke('#ffffff');
+  noFill();
+  circle(300, 150, 6);
+  
+  //중간하단작은별
+  
+  stroke('#ffffff');
+  fill('#ffffff');
+  circle(380, 100, 3);
+  
+  stroke('#ffffff');
+  noFill();
+  circle(380, 100, 6);
+  
+  //왼쪽중단작은별
+  
+  stroke('#ffffff');
+  fill('#ffffff');
+  circle(450, 40, 3);
+  
+  stroke('#ffffff');
+  noFill();
+  circle(450, 40, 6);
+  
+  //중간상단작은별
+  
+  stroke('#ffffff');
+  fill('#ffffff');
+  circle(560, 110, 3);
+  
+  stroke('#ffffff');
+  noFill();
+  circle(560, 110, 6);
+  
+  //제일오른쪽작은별
+  //작은별관련코드
+  
+  stroke('#002600');
+  strokeWeight(1);
+  fill('#002600')
+  triangle(600, 200, 600, 400, 155, 400);
+  
+  stroke('#003300');
+  line(600, 200, 600, 400);
+  
+  stroke('#003300');
+  line(600, 200, 595, 400);
+  
+  stroke('#003300');
+  line(600, 200, 590, 400);
+  
+  stroke('#003300');
+  line(600, 200, 585, 400);
+  
+  stroke('#003300');
+  line(600, 200, 580, 400);
+  
+  stroke('#003300');
+  line(600, 200, 575, 400);
+  
+  stroke('#003300');
+  line(600, 200, 570, 400);
+
+  stroke('#003300');
+  line(600, 200, 565, 400);
+
+  stroke('#003300');
+  line(600, 200, 560, 400);
+
+  stroke('#003300');
+  line(600, 200, 555, 400);
+  
+  stroke('#004d00');
+  line(600, 200, 550, 400);
+  
+  stroke('#004d00');
+  line(600, 200, 545, 400);
+  
+  stroke('#004d00');
+  line(600, 200, 540, 400);
+  
+  stroke('#004d00');
+  line(600, 200, 535, 400);
+  
+  stroke('#004d00');
+  line(600, 200, 530, 400);
+  
+  stroke('#004d00');
+  line(600, 200, 525, 400);
+  
+  stroke('#004d00');
+  line(600, 200, 520, 400);
+  
+  stroke('#004d00');
+  line(600, 200, 515, 400);
+  
+  stroke('#004d00');
+  line(600, 200, 510, 400);
+  
+  stroke('#004d00');
+  line(600, 200, 505, 400);
+  
+  stroke('#0A6E0A');
+  line(600, 200, 500, 400);
+
+  stroke('#0A6E0A');
+  line(600, 200, 495, 400);
+  
+  stroke('#0A6E0A');
+  line(600, 200, 490, 400);
+  
+  stroke('#0A6E0A');
+  line(600, 200, 485, 400);
+  
+  stroke('#0A6E0A');
+  line(600, 200, 480, 400);
+  
+  stroke('#0A6E0A');
+  line(600, 200, 475, 400);
+  
+  stroke('#0A6E0A');
+  line(600, 200, 470, 400);
+  
+  stroke('#0A6E0A');
+  line(600, 200, 465, 400);
+  
+  stroke('#0A6E0A');
+  line(600, 200, 460, 400);
+  
+  stroke('#0A6E0A');
+  line(600, 200, 455, 400);
+  
+  stroke('#228B22');
+  line(600, 200, 450, 400);
+
+  stroke('#228B22');
+  line(600, 200, 445, 400);
+
+  stroke('#228B22');
+  line(600, 200, 440, 400);
+
+  stroke('#228B22');
+  line(600, 200, 435, 400);
+
+  stroke('#228B22');
+  line(600, 200, 430, 400);
+  
+  stroke('#228B22');
+  line(600, 200, 425, 400);
+
+  stroke('#228B22');
+  line(600, 200, 420, 400);
+
+  stroke('#228B22');
+  line(600, 200, 415, 400);
+
+  stroke('#228B22');
+  line(600, 200, 410, 400);
+
+  stroke('#228B22');
+  line(600, 200, 405, 400);
+
+  stroke('#32CD32');
+  line(600, 200, 400, 400);
+  
+  stroke('#32CD32');
+  line(600, 200, 395, 400);
+  
+  stroke('#32CD32');
+  line(600, 200, 390, 400);
+  
+  stroke('#32CD32');
+  line(600, 200, 385, 400);
+  
+  stroke('#32CD32');
+  line(600, 200, 380, 400);
+  
+  stroke('#32CD32');
+  line(600, 200, 375, 400);
+  
+  stroke('#32CD32');
+  line(600, 200, 370, 400);
+  
+  stroke('#32CD32');
+  line(600, 200, 365, 400);
+  
+  stroke('#32CD32');
+  line(600, 200, 360, 400);
+  
+  stroke('#32CD32');
+  line(600, 200, 355, 400);
+  
+  stroke('#7CFC00');
+  line(600, 200, 350, 400);
+  
+  stroke('#7CFC00');
+  line(600, 200, 345, 400);
+  
+  stroke('#7CFC00');
+  line(600, 200, 340, 400);
+  
+  stroke('#7CFC00');
+  line(600, 200, 335, 400);
+  
+  stroke('#7CFC00');
+  line(600, 200, 330, 400);
+  
+  stroke('#7CFC00');
+  line(600, 200, 325, 400);
+  
+  stroke('#7CFC00');
+  line(600, 200, 320, 400);
+  
+  stroke('#7CFC00');
+  line(600, 200, 315, 400);
+  
+  stroke('#7CFC00');
+  line(600, 200, 310, 400);
+  
+  stroke('#7CFC00');
+  line(600, 200, 305, 400);
+  
+  stroke('#ADFF2F');
+  line(600, 200, 300, 400);
+  
+  stroke('#ADFF2F');
+  line(600, 200, 295, 400);
+  
+  stroke('#ADFF2F');
+  line(600, 200, 290, 400);
+  
+  stroke('#ADFF2F');
+  line(600, 200, 285, 400);
+  
+  stroke('#ADFF2F');
+  line(600, 200, 280, 400);
+  
+  stroke('#ADFF2F');
+  line(600, 200, 275, 400);
+  
+  stroke('#ADFF2F');
+  line(600, 200, 270, 400);
+  
+  stroke('#ADFF2F');
+  line(600, 200, 265, 400);
+  
+  stroke('#ADFF2F');
+  line(600, 200, 260, 400);
+  
+  stroke('#ADFF2F');
+  line(600, 200, 255, 400);
+  
+  stroke('#CCFF66');
+  line(600, 200, 250, 400);
+  
+  stroke('#CCFF66');
+  line(600, 200, 245, 400);
+  
+  stroke('#CCFF66');
+  line(600, 200, 240, 400);
+  
+  stroke('#CCFF66');
+  line(600, 200, 235, 400);
+  
+  stroke('#CCFF66');
+  line(600, 200, 230, 400);
+  
+  stroke('#CCFF66');
+  line(600, 200, 225, 400);
+  
+  stroke('#CCFF66');
+  line(600, 200, 220, 400);
+  
+  stroke('#CCFF66');
+  line(600, 200, 215, 400);
+  
+  stroke('#CCFF66');
+  line(600, 200, 210, 400);
+  
+  stroke('#CCFF66');
+  line(600, 200, 205, 400);
+  
+  stroke('#E6FFB3');
+  line(600, 200, 200, 400);
+  
+  stroke('#E6FFB3');
+  line(600, 200, 195, 400);
+  
+  stroke('#E6FFB3');
+  line(600, 200, 190, 400);
+  
+  stroke('#E6FFB3');
+  line(600, 200, 185, 400);
+  
+  stroke('#E6FFB3');
+  line(600, 200, 180, 400);
+  
+  stroke('#E6FFB3');
+  line(600, 200, 175, 400);
+  
+  stroke('#E6FFB3');
+  line(600, 200, 170, 400);
+  
+  stroke('#E6FFB3');
+  line(600, 200, 165, 400);
+  
+  stroke('#E6FFB3');
+  line(600, 200, 160, 400);
+  
+  stroke('#E6FFB3');
+  line(600, 200, 155, 400);
+  
+  //산
+  
+  stroke('#8B4513');
+  strokeWeight(1);
+  fill('#8B4513');
+  quad(60, 360, 80, 360, 80, 400, 60, 400);
+  
+  stroke('#D2B48C');
+  line(80, 360, 80, 400);
+  
+  stroke('#D2B48C');
+  line(78, 360, 78, 400);
+  
+  stroke('#D2B48C');
+  line(76, 360, 76, 400);
+  
+  stroke('#CD853F');
+  line(74, 360, 74, 400);
+  
+  stroke('#CD853F');
+  line(72, 360, 72, 400);
+  
+  stroke('#CD853F');
+  line(70, 360, 70, 400);
+  
+  stroke('#A0522D');
+  line(68, 360, 68, 400);
+  
+  stroke('#A0522D');
+  line(66, 360, 66, 400);
+  
+  stroke('#A0522D');
+  line(64, 360, 64, 400);
+  
+  //나무기둥
+  
+  stroke('#FF69B4');
+  strokeWeight(1);
+  fill('#FF69B4');
+  quad(100, 310, 130, 340, 100, 370, 70, 340);
+
+  stroke('#FFF0F5');
+  line(100, 310, 130, 340);
+
+  stroke('#FFF0F5');
+  line(98, 312, 128, 342);
+
+  stroke('#FFF0F5');
+  line(96, 314, 126, 344);
+
+  stroke('#FFF0F5');
+  line(94, 316, 124, 346);
+
+  stroke('#FADADD');
+  line(92, 318, 122, 348);
+
+  stroke('#FADADD');
+  line(90, 320, 120, 350);
+
+  stroke('#FADADD');
+  line(88, 322, 118, 352);
+
+  stroke('#FADADD');
+  line(86, 324, 116, 354);
+
+  stroke('#FFB6C1');
+  line(84, 326, 114, 356);
+
+  stroke('#FFB6C1');
+  line(82, 328, 112, 358);
+
+  stroke('#FFB6C1');
+  line(80, 330, 110, 360);
+
+  stroke('#FFB6C1');
+  line(78, 332, 108, 362);
+  
+  //왼쪽잎
+  
+  stroke('#FF69B4');
+  strokeWeight(1);
+  fill('#FF69B4');
+  quad(70, 310, 100, 340, 70, 370, 40, 340);
+  
+  stroke('#FFF0F51');
+  line(70, 310, 100, 340);
+  
+  stroke('#FFF0F51');
+  line(68, 312, 98, 342);
+  
+  stroke('#FFF0F51');
+  line(66, 314, 96, 344);
+  
+  stroke('#FFF0F51');
+  line(64, 316, 94, 346);
+  
+  stroke('#FADADD');
+  line(62, 318, 92, 348);
+  
+  stroke('#FADADD');
+  line(60, 320, 90, 350);
+  
+  stroke('#FADADD');
+  line(58, 322, 88, 352);
+  
+  stroke('#FADADD');
+  line(56, 324, 86, 354);
+  
+  stroke('#FFB6C1');
+  line(54, 326, 84, 356);
+  
+  stroke('#FFB6C1');
+  line(52, 328, 82, 358);
+  
+  stroke('#FFB6C1');
+  line(50, 330, 80, 360);
+  
+  stroke('#FFB6C1');
+  line(48, 332, 78, 362);
+  
+  //가운데잎
+  
+  stroke('#FF69B4');
+  strokeWeight(1);
+  fill('#FF69B4');
+  quad(40, 310, 70, 340, 40, 370, 10, 340);
+  
+  stroke('#FFF0F5');
+  strokeWeight(1);
+  line(40, 310, 70, 340);
+
+  stroke('#FFF0F5');
+  line(38, 312, 68, 342);
+
+  stroke('#FFF0F5');
+  line(36, 314, 66, 344);
+
+  stroke('#FFF0F5');
+  line(34, 316, 64, 346);
+
+  stroke('#FADADD');
+  line(32, 318, 62, 348);
+
+  stroke('#FADADD');
+  line(30, 320, 60, 350);
+
+  stroke('#FADADD');
+  line(28, 322, 58, 352);
+
+  stroke('#FADADD');
+  line(26, 324, 56, 354);
+
+  stroke('#FFB6C1');
+  line(24, 326, 54, 356);
+
+  stroke('#FFB6C1');
+  line(22, 328, 52, 358);
+
+  stroke('#FFB6C1');
+  line(20, 330, 50, 360);
+
+  stroke('#FFB6C1');
+  line(18, 332, 48, 362);
+  
+  //오른쪽잎
+  
+  stroke('#FF69B4');
+  strokeWeight(1);
+  fill('#FF69B4');
+  quad(85, 275, 115, 305, 85, 335, 55, 305);
+
+  stroke('#FFF0F51');
+  line(85, 275, 115, 305);
+
+  stroke('#FFF0F51');
+  line(83, 277, 113, 307);
+
+  stroke('#FFF0F51');
+  line(81, 279, 111, 309);
+
+  stroke('#FFF0F51');
+  line(79, 281, 109, 311);
+
+  stroke('#FADADD');
+  line(77, 283, 107, 313);
+
+  stroke('#FADADD');
+  line(75, 285, 105, 315);
+
+  stroke('#FADADD');
+  line(73, 287, 103, 317);
+
+  stroke('#FADADD');
+  line(71, 289, 101, 319);
+
+  stroke('#FFB6C1');
+  line(69, 291, 99, 321);
+
+  stroke('#FFB6C1');
+  line(67, 293, 97, 323);
+
+  stroke('#FFB6C1');
+  line(65, 295, 95, 325);
+
+  stroke('#FFB6C1');
+  line(63, 297, 93, 327);
+  
+  //위오른잎
+  
+  stroke('#FF69B4');
+  strokeWeight(1);
+  fill('#FF69B4');
+  quad(55, 275, 85, 305, 55, 335, 25, 305);
+
+  stroke('#FFF0F51');
+  line(55, 275, 85, 305);
+
+  stroke('#FFF0F51');
+  line(53, 277, 83, 307);
+
+  stroke('#FFF0F51');
+  line(51, 279, 81, 309);
+
+  stroke('#FFF0F51');
+  line(49, 281, 79, 311);
+
+  stroke('#FADADD');
+  line(47, 283, 77, 313);
+
+  stroke('#FADADD');
+  line(45, 285, 75, 315);
+
+  stroke('#FADADD');
+  line(43, 287, 73, 317);
+
+  stroke('#FADADD');
+  line(41, 289, 71, 319);
+
+  stroke('#FFB6C1');
+  line(39, 291, 69, 321);
+
+  stroke('#FFB6C1');
+  line(37, 293, 67, 323);
+
+  stroke('#FFB6C1');
+  line(35, 295, 65, 325);
+
+  stroke('#FFB6C1');
+  line(33, 297, 63, 327);
+  
+  //위왼쪽잎
+  
+  rectMode(CENTER);
+
+  stroke('#dbefff');
+  strokeWeight(1);
+  fill('#dbefff');
+  rect(150 + 90, 220 + 10, 10, 2, 1);
+  
+  stroke('#dbefff');
+  noFill();
+  rect(150 + 90, 220 + 10, 15, 7, 1);
+  
+  stroke('#dbefff');
+  noFill();
+  rect(150 + 90, 220 + 10, 20, 12, 2);
+  
+  stroke('#dbefff');
+  noFill();
+  rect(150 + 90, 220 + 10, 25, 17, 3);
+  
+  stroke('#a3cdee');
+  noFill();
+  rect(150 + 90, 220 + 10, 30, 22, 4);
+
+  stroke('#a3cdee');
+  noFill();
+  rect(150 + 90, 220 + 10, 35, 27, 5);
+  
+  stroke('#a3cdee');
+  noFill();
+  rect(150 + 90, 220 + 10, 40, 32, 6);
+  
+  stroke('#6ea1c9');
+  noFill();
+  rect(150 + 90, 220 + 10, 45, 37, 7);
+  
+  stroke('#6ea1c9');
+  noFill();
+  rect(150 + 90, 220 + 10, 50, 42, 8);
+  
+  stroke('#6ea1c9');
+  noFill();
+  rect(150 + 90, 220 + 10, 55, 47, 9);
+  
+  stroke('#6ea1c9');
+  line(178 + 90, 210 + 10, 210 + 90, 210 + 10);
+  
+  stroke('#6ea1c9');
+  line(178 + 90, 215 + 10, 215 + 90, 215 + 10);
+  
+  stroke('#6ea1c9');
+  line(178 + 90, 220 + 10, 220 + 90, 220 + 10);
+  
+  stroke('#6ea1c9');
+  line(178 + 90, 225 + 10, 225 + 90, 225 + 10);
+  
+  stroke('#6ea1c9');
+  line(178 + 90, 230 + 10, 230 + 90, 230 + 10);
+  
+  //달아래구름
+  
+  rectMode(CENTER);
+  
+  stroke('#dbefff');
+  strokeWeight(1);
+  fill('#dbefff');
+  rect(400, 170, 10, 2, 1);
+  
+  stroke('#dbefff');
+  noFill();
+  rect(400, 170, 15, 7, 1);
+  
+  stroke('#dbefff');
+  noFill();
+  rect(400, 170, 20, 12, 2);
+  
+  stroke('#dbefff');
+  noFill();
+  rect(400, 170, 25, 17, 3);
+  
+  stroke('#a3cdee');
+  noFill();
+  rect(400, 170, 30, 22, 4);
+  
+  stroke('#a3cdee');
+  noFill();
+  rect(400, 170, 35, 27, 5);
+  
+  stroke('#a3cdee');
+  noFill();
+  rect(400, 170, 40, 32, 6);
+  
+  stroke('#6ea1c9');
+  noFill();
+  rect(400, 170, 45, 37, 7);
+  
+  stroke('#6ea1c9');
+  noFill();
+  rect(400, 170, 50, 42, 8);
+  
+  stroke('#6ea1c9');
+  noFill();
+  rect(400, 170, 55, 47, 9);
+  
+  stroke('#6ea1c9');
+  line(428, 160, 500, 160);
+  
+  stroke('#6ea1c9');
+  line(428, 165, 480, 165);
+  
+  stroke('#6ea1c9');
+  line(428, 170, 470, 170);
+  
+  stroke('#6ea1c9');
+  line(428, 175, 460, 175);
+  
+  stroke('#6ea1c9');
+  line(428, 180, 450, 180);
+  
+  //달옆구름
+  
+}
